fed542ac8f46d58c5d1bd2abe592793a14b73db2
nestor9623 <nestor9623@hotmail.com>
2020-02-28 11:45:39 +0100

    Consumo de web service desde angular con rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
+import { ShareModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaComponent } from './usuarios/lista/lista.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ShareModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+
+
+export class Usuario {
+
+    constructor(public id: string,
+                // tslint:disable-next-line: variable-name
+                public first_name: string,
+                // tslint:disable-next-line: variable-name
+                public last_name: string,
+                public avatar: string) {
+
+    }
+}
